dfbfcb8b92703d52f2e8c9992e9bd4ccffbbeaa6
Paweł Walnicki <pawel.walnicki@gmail.com>
2016-02-15 18:05:24 +0100

    zadanie 1

---
+++ zad_1/zad1/src/zad1/Zad1.java
@@ -0,0 +1,13 @@
+package zad1;
+
+public class Zad1 {
+
+    public static void main(String[] args) {
+        //String imie = "Pawel", nazwisko = "Walnicki";
+        //int x = 5;
+        //System.out.printf("Witaj %s %s %d!\n\tTekst drugiej linii\n", imie, nazwisko, x);
+        
+        System.out.println("Witaj świecie!");
+    }
+    
+}
